aea82e50975666258d383ab5254d67a1d90b6343
Lucas Azevedo <lucas@axiontecnologia.com.br>
2024-04-12 16:27:15 -0300

    Correção no metodo rest api

--- EntregaTudo/EntregaTudo.Api/Controllers/Base/RestApiControllerBase.cs
+++ EntregaTudo/EntregaTudo.Api/Controllers/Base/RestApiControllerBase.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntregaTudo.Shared.Dto.Base;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace EntregaTudo.Api.Controllers.Base;
 
-//Verificar essa classe;
 [ApiController]
-public abstract class RestApiControllerBase<TEntity, TContext> : Controller
+public abstract class RestApiControllerBase<TEntity, TDto, TContext> : Controller
     where TEntity : class
+    where TDto : DtoBase
     where TContext : DbContext
 {
     private readonly TContext _context;
@@ -14,7 +15,16 @@ public abstract class RestApiControllerBase<TEntity, TContext> : Controller
     {
         _context = context;
     }
-    
+
+    [NonAction]
+    protected abstract TEntity ToDomain(TDto dto, TEntity? entity = null);
+
+    [NonAction]
+    protected abstract TDto ToDto(TEntity domain);
+
+    [NonAction]
+    protected TEntity? GetDomain(TDto dto) => _context.Set<TEntity>().Find(dto.Id);
+
     [HttpGet]
     public virtual async Task<IActionResult> Get()
     {
@@ -22,13 +32,111 @@ public abstract class RestApiControllerBase<TEntity, TContext> : Controller
         {
             var result = await _context.Set<TEntity>()
                 .ToListAsync();
-                
+
             return Ok(result);
         }
         catch (Exception e)
         {
-          //  Logger.LogError(e.Message);
             return BadRequest(e.Message);
         }
     }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public virtual async Task<IActionResult> Get(Guid? id)
+    {
+        var context = await _context.Set<TEntity>().FindAsync(id);
+
+        if (context != null)
+            return Ok(context);
+
+        return Ok();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public virtual async Task<IActionResult> Post([FromBody] TDto value)
+    {
+        try
+        {
+            if (value == null)
+                return BadRequest($"O objeto[{nameof(TDto)}] não deve ser nulo.");
+
+            TEntity domain;
+
+            try
+            {
+                domain = ToDomain(value);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            await _context.Set<TEntity>().AddAsync(domain);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(ToDto(domain));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public virtual async Task<IActionResult> Put(Guid? id, [FromBody] TDto dto)
+    {
+        try
+        {
+            if (!id.HasValue)
+                return NotFound();
+
+            dto.Id = id;
+
+            var entity = GetDomain(dto);
+
+            if (entity == null) return Ok();
+
+           var domain = ToDomain(dto, entity);
+
+           _context.ChangeTracker.Clear();
+
+            _context.Update(domain);
+            await _context.SaveChangesAsync();
+
+            return Ok(ToDto(entity));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public virtual async Task<IActionResult> Delete(Guid? id)
+    {
+        if (id == null)
+            return BadRequest("Id não pode ser nulo!");
+
+        var entity = await _context.Set<TEntity>().FindAsync(id);
+
+        if (entity == null) return Ok();
+
+        _context.Set<TEntity>().Remove(entity);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
 }
--- EntregaTudo/EntregaTudo.Api/Controllers/PersonController.cs
+++ EntregaTudo/EntregaTudo.Api/Controllers/PersonController.cs
@@ -1,16 +1,45 @@
 using EntregaTudo.Api.Controllers.Base;
+using EntregaTudo.Core.Domain.Enum;
 using EntregaTudo.Core.Domain.User;
 using EntregaTudo.Dal.Context;
+using EntregaTudo.Shared.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EntregaTudo.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class PersonController : RestApiControllerBase<Person, EntregaTudoDbContext>
+public class PersonController : RestApiControllerBase<Person, PersonDto, EntregaTudoDbContext>
 {
     public PersonController(EntregaTudoDbContext context) : base(context)
     {
     }
 
+    protected override Person ToDomain(PersonDto dto, Person? person = null)
+    {
+        return new Person
+        {
+            Id = dto.Id,
+            FirstName = dto.FirstName ?? person?.FirstName,
+            DocumentNumber = dto.DocumentNumber ?? person?.DocumentNumber,
+            Email = dto.Email ?? person?.Email,
+            LastName = dto.LastName ?? person?.LastName,
+            PersonType = (PersonType?)dto.PersonType ?? PersonType.User,
+            PhoneNumber = dto.PhoneNumber ?? person?.PhoneNumber
+        };
+    }
+
+    protected override PersonDto ToDto(Person domain)
+    {
+        return new PersonDto
+        {
+            Id = domain.Id,
+            FirstName = domain.FirstName,
+            DocumentNumber = domain.DocumentNumber,
+            Email = domain.Email,
+            LastName = domain.LastName,
+            PersonType = (Shared.Enums.PersonType)domain.PersonType,
+            PhoneNumber = domain.PhoneNumber  
+        };
+    }
 }
