236a1d4cdc38f717d446143fffc3daf0e9bac944
Lucas Azevedo <lucas@axiontecnologia.com.br>
2024-04-12 16:43:32 -0300

    Cadastro de veiculo

---
+++ EntregaTudo/EntregaTudo.Api/Controllers/VehicleController.cs
@@ -0,0 +1,49 @@
+﻿using EntregaTudo.Api.Controllers.Base;
+using EntregaTudo.Core.Domain.Base;
+using EntregaTudo.Core.Domain.Business.Vehicle;
+using EntregaTudo.Core.Domain.Enum;
+using EntregaTudo.Dal.Context;
+using EntregaTudo.Shared.Dto;
+using Microsoft.AspNetCore.Mvc;
+using VehicleType = EntregaTudo.Shared.Enums.VehicleType;
+
+namespace EntregaTudo.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VehicleController : RestApiControllerBase<Vehicle, VehicleDto, EntregaTudoDbContext>
+{
+    public VehicleController(EntregaTudoDbContext context) : base(context)
+    {
+    }
+
+    protected override Vehicle ToDomain(VehicleDto dto, Vehicle? entity = null)
+    {
+        return new Vehicle
+        {
+            Id = dto.Id,
+            Brand = dto.Brand ?? entity?.Brand,
+            LicensePlate = dto.LicensePlate ?? entity?.LicensePlate,
+            LoadCapacity = dto.LoadCapacity ?? entity?.LoadCapacity,
+            ManufactureYear = dto.ManufactureYear ?? entity?.ManufactureYear,
+            Model = dto.Model ?? entity?.Model,
+            VehicleStatus = (VehicleStatus?)dto.VehicleStatus ?? VehicleStatus.Available,
+            VehicleType = (Core.Domain.Enum.VehicleType?)(dto.VehicleType ?? VehicleType.Motorcycle)
+        };
+    }
+
+    protected override VehicleDto ToDto(Vehicle domain)
+    {
+        return new VehicleDto
+        {
+            Id = domain.Id,
+            Brand = domain.Brand,
+            LicensePlate = domain.LicensePlate,
+            LoadCapacity = domain.LoadCapacity,
+            ManufactureYear = domain.ManufactureYear,
+            Model = domain.Model,
+            VehicleStatus = (Shared.Enums.VehicleStatus?)domain.VehicleStatus,
+            VehicleType =(VehicleType?)domain.VehicleType
+        };
+    }
+}
---
+++ EntregaTudo/EntregaTudo.Shared/Dto/VehicleDto.cs
@@ -0,0 +1,41 @@
+﻿using EntregaTudo.Shared.Dto.Base;
+using EntregaTudo.Shared.Enums;
+
+namespace EntregaTudo.Shared.Dto;
+
+public class VehicleDto : DtoBase
+{
+    /// <summary>
+    /// Marca
+    /// </summary>
+    public string? Brand { get; set; }
+
+    /// <summary>
+    /// Modelo
+    /// </summary>
+    public string? Model { get; set; }
+
+    /// <summary>
+    /// Ano Fabricacao
+    /// </summary>
+    public int? ManufactureYear { get; set; }
+
+    /// <summary>
+    /// Placa do Veículo
+    /// </summary>
+    public string? LicensePlate { get; set; }
+    /// <summary>
+    /// Capacidade Carga
+    /// </summary>
+    public double? LoadCapacity { get; set; }
+
+    /// <summary>
+    /// Status do Veículo, se pode fazer entregas ou não
+    /// </summary>
+    public VehicleStatus? VehicleStatus { get; set; }
+
+    /// <summary>
+    /// Tipo do Veículo
+    /// </summary>
+    public VehicleType? VehicleType { get; set; }
+}
