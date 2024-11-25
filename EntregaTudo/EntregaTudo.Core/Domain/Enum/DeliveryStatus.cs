2319ff720d458d6b057f7b638eb017875ec40b37
Lucas Azevedo <lucas@axiontecnologia.com.br>
2024-11-25 16:41:51 -0300

    feature: Adicionado display name no enum de deliveryStatus

--- EntregaTudo/EntregaTudo.Core/Domain/Enum/DeliveryStatus.cs
+++ EntregaTudo/EntregaTudo.Core/Domain/Enum/DeliveryStatus.cs
@@ -1,8 +1,13 @@
-﻿namespace EntregaTudo.Core.Domain.Enum;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntregaTudo.Core.Domain.Enum;
 
 public enum DeliveryStatus
 {
+    [Display(Name = "Entregue")]
     Ok,
+    [Display(Name = "Aguardando Envio")]
     Pending,
+    [Display(Name = "Em Trânsito")]
     Sended,
 }
